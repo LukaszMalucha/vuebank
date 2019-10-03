7e859f3be0b8e78cb31ac20300ff1f1a9d424a6c
unknown <lucasmalucha@gmail.com>
2019-10-03 07:20:19 +0100

    nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app">
-    <NavbarComponent />
     <router-view/>
   </div>
 </template>
 
 <script>
-import NavbarComponent from "./components/Navbar.vue"
-export default {
-    name: "App",
-    components: {
-        NavbarComponent
-    }
-}
+
 </script>
 
 <style>
