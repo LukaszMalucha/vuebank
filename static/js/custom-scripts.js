a7cf4e37ae5f883cae5540f88f8a6195ae879d3b
unknown <lucasmalucha@gmail.com>
2019-10-03 19:27:40 +0100

    form

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -1,9 +1,9 @@
 $('.dropdown-trigger').dropdown();
 
 
-$(".alert").delay(4000).fadeOut(300, function() {
-    $(this).alert('close');
-});
+//$(".alert").delay(4000).fadeOut(300, function() {
+//    $(this).alert('close');
+//});
 
 
 $(document).ready(function() {
@@ -17,10 +17,6 @@ $(document).ready(function() {
 
     $('#selectArea').formSelect();
 
-    $('.counter').counterUp({
-        delay: 10,
-        time: 400
-    });
 });
 
 
