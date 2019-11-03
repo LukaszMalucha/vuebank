a583c59eb434b84c5d2f705cb2badd66b1947768
unknown <lucasmalucha@gmail.com>
2019-11-03 21:19:27 +0000

    nav

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer">
         <p><a target="_blank" href="https://www.freeprivacypolicy.com/privacy/view/9b972ff9a2aaeda03f07216c15625791/">Privacy
-            Policy</a> • © 2019 - 2020 www.unflasked.com - All Rights Reserved • Powered by Septellar &trade;</p>
+            Policy</a> • © 2019 - 2020 www.septellar.com - All Rights Reserved • Powered by Septellar &trade;</p>
     </div>
 </template>
 
