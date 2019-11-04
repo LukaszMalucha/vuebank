f1f714eb33bf6ec6f4b3fd562b942605cb8e0340
unknown <lucasmalucha@gmail.com>
2019-11-04 07:08:47 +0000

    home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,51 @@
 <template>
-  <div id="page-index">
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="wrapper">
+<div class="container text-center container-welcome">
+        <div class="row">
+            <h3>Welcome to the RESTBank API</h3>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <button class="btn route"><b>Register</b></button>
+            </div>
+            <div class="col-md-3 text-center">
+                <button class="btn route"><b>Authenticate</b>
+                </button>
+            </div>
+            <div class="col-md-3 text-center">
+                <button class="btn route"><b>User Details</b>
+                </button>
+            </div>
+            <div class="col-md-3 text-center">
+                <button class="btn route"><b>My
+                    Portfolio</b></button>
+            </div>
+        </div>
+        <br>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { apiService } from "../common/api.service"
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+        questions: []
+    }
+
+  },
+  methods: {
+    getQuestions() {
+        let endpoint = "api/questions/";
+        apiService(endpoint)
+        .then(data => {
+            this.questions.push(...data.results);
+        })
+    }
   }
 }
 </script>
