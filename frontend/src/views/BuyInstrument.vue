cd4952b8d4bc0589c5918ee7b96b1a5370fb17b6
unknown <lucasmalucha@gmail.com>
2019-11-22 18:40:37 +0000

    404

--- frontend/src/views/BuyInstrument.vue
+++ frontend/src/views/BuyInstrument.vue
@@ -1,70 +1,81 @@
 <template>
-  <div class="wrapper">
-    <div id="page-index">
-      <div class="dashboard-cards">
-        <div class="row">
-          <div class="col-md-4 plain-element"></div>
-          <div class="col-md-4 plain-element">
-            <div class="card card-instrument">
-              <div class="card-header">
-                <div class="row plain-element">
-                  <div class="col-md-4 plain-element">
-                    <div class="card-image">
-                      <img alt="Currency" src="../assets/currency.png" class="img responsive"/>
-                    </div>
-                  </div>
-                  <div class="col-md-8 plain-element">
-                    <div class="card-title text-left">
-                      <h5>{{ instrument.name }}</h5>
-                        <table class="table table-company">
-                          <tbody>
-                            <tr>
-                              <td>Category:</td>
-                              <td><b>{{ instrument.category }}</b></td>
-                            </tr>
-                            <tr>
-                              <td>Ticker:</td>
-                              <td><b>{{ instrument.symbol }}</b></td>
-                            </tr>
-                            <tr>
-                              <td>Current Price:</td>
-                              <td><b>{{ instrument.price }} USD</b></td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
+<div class="wrapper">
+  <div id="page-index">
+    <div v-if="instrument" class="dashboard-cards">
+      <div class="row">
+        <div class="col-md-4 plain-element"></div>
+        <div class="col-md-4 plain-element">
+          <div class="card card-instrument">
+            <div class="card-header">
+              <div class="row plain-element">
+                <div class="col-md-4 plain-element">
+                  <div class="card-image">
+                    <img alt="Currency" src="../assets/currency.png" class="img responsive"/>
                   </div>
                 </div>
+                <div class="col-md-8 plain-element">
+                  <div class="card-title text-left">
+                    <h5>{{ instrument.name }}</h5>
+                    <table class="table table-company">
+                      <tbody>
+                      <tr>
+                        <td>Category:</td>
+                        <td><b>{{ instrument.category }}</b></td>
+                      </tr>
+                      <tr>
+                        <td>Ticker:</td>
+                        <td><b>{{ instrument.symbol }}</b></td>
+                      </tr>
+                      <tr>
+                        <td>Current Price:</td>
+                        <td><b>{{ instrument.price }} USD</b></td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div class="card-content">
               <b class="instrument-website">
                 <router-link :to="{ name: 'instrument', params: { slug: instrument.slug} }">
-                    Buy
+                  Buy
                 </router-link>
               </b>
             </div>
           </div>
         </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4 plain-element"></div>
-          <div class="col-md-4 plain-element">
-            <div class="card text-center">
+      </div>
+      <div class="row">
+        <div class="col-md-4 plain-element"></div>
+        <div class="col-md-4 plain-element">
+          <div class="card text-center">
+            <br>
+            <form @submit.prevent="onSubmit">
               <br>
-              <form @submit.prevent="onSubmit">
-                <br>
-                <input v-model="assetQuantity" type="number" placeholder="quantity" class="form-control">
-                </input>
-                <br>
-                <button type="submit" class="btn btn-success">
-                  Buy
-                </button>
-              </form>
-              <p v-if="error" class="muted">{{ error }}</p>
+              <input v-model="assetQuantity" type="number" placeholder="quantity" class="form-control">
+              </input>
+              <br>
+              <button type="submit" class="btn btn-success">
+                Buy
+              </button>
+            </form>
+            <p v-if="error" class="muted">{{ error }}</p>
+          </div>
+        </div>
+        <div class="col-md-4 plain-element"></div>
+      </div>
+    </div>
+    <div v-else class="dashboard-cards">
+      <div class="row">
+        <div class="col-md-4 plain-element"></div>
+        <div class="col-md-4 plain-element">
+          <div class="card card-instrument">
+            <div class="card-content">
+              <h4> 404 - Page Not Found </h4>
             </div>
           </div>
-          <div class="col-md-4 plain-element"></div>
-          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -99,15 +110,20 @@ export default {
       let endpoint = `/portfolio/instruments/${this.slug}/`;
       apiService(endpoint)
         .then(data => {
-          this.instrument = data;
-          this.setPageTitle(data.name)
+          if (data) {
+            this.instrument = data;
+            this.setPageTitle(data.name)
+          } else {
+            this.instrument = null;
+            this.setPageTitle("404 - Page Not Found")
+          }
         })
     },
     onSubmit() {
       let endpoint = "/portfolio/buy/";
       let method = "POST";
       apiService(endpoint, method, { quantity: this.assetQuantity, symbol: this.instrument.symbol, instrument: this.instrument.id })
-        .then(cash_data => {
+        .then(data => {
           this.$router.push({
           name: 'cash-balance',
           })
