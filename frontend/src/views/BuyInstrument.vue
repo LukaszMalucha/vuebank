b6d9a930e742e07b074c10bfa58ea34cce3958dd
unknown <lucasmalucha@gmail.com>
2019-11-25 21:28:25 +0000

    buy form

--- frontend/src/views/BuyInstrument.vue
+++ frontend/src/views/BuyInstrument.vue
@@ -3,7 +3,7 @@
   <RowHeaderComponent/>
   <div class="row row-banner row-banner-small">
     <div class="col-md-8 text-left col-banner-small no-padding">
-    <h4>Buy  {{ instrument.name }}</h4>
+      <h4>Buy {{ instrument.name }}</h4>
     </div>
     <div class="col-md-4 no-padding">
     </div>
@@ -12,74 +12,49 @@
     <div class="row">
       <div class="col-md-4 plain-element"></div>
       <div class="col-md-4 plain-element">
-        <div class="card card-instrument">
-          <div class="card-header">
-            <div class="row plain-element">
-              <div class="col-md-4 plain-element">
-                <div class="card-image">
-                  <img alt="Currency" src="../assets/currency.png" class="img responsive"/>
-                </div>
-              </div>
-              <div class="col-md-8 plain-element">
-                <div class="card-title text-left">
-                  <h5>{{ instrument.name }}</h5>
-                  <table class="table table-company">
-                    <tbody>
-                    <tr>
-                      <td>Category:</td>
-                      <td><b>{{ instrument.category }}</b></td>
-                    </tr>
-                    <tr>
-                      <td>Ticker:</td>
-                      <td><b>{{ instrument.symbol }}</b></td>
-                    </tr>
-                    <tr>
-                      <td>Current Price:</td>
-                      <td><b>{{ instrument.price }} USD</b></td>
-                    </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card-content">
-            <b class="instrument-website">
-              <router-link :to="{ name: 'instrument', params: { slug: instrument.slug} }">
+        <table class="table table-company">
+          <tbody>
+          <tr>
+            <td>Category:</td>
+            <td><b>{{ instrument.category }}</b></td>
+          </tr>
+          <tr>
+            <td>Ticker:</td>
+            <td><b>{{ instrument.symbol }}</b></td>
+          </tr>
+          <tr>
+            <td>Current Price:</td>
+            <td><b>{{ instrument.price }} USD</b></td>
+          </tr>
+
+          <tr>
+          <div class="card text-center">
+            <form @submit.prevent="onSubmit">
+              <br>
+              <input v-model="assetQuantity" type="number" placeholder="quantity" class="form-control"/>
+
+              <br>
+              <button type="submit" class="btn btn-success">
                 Buy
-              </router-link>
-            </b>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-4 plain-element"></div>
-      <div class="col-md-4 plain-element">
-        <div class="card text-center">
-          <br>
-          <form @submit.prevent="onSubmit">
-            <br>
-            <input v-model="assetQuantity" type="number" placeholder="quantity" class="form-control">
-            </input>
-            <br>
-            <button type="submit" class="btn btn-success">
-              Buy
-            </button>
-          </form>
-          <p v-if="error" class="muted">{{ error }}</p>
-        </div>
+              </button>
+            </form>
+            </div>
+            <p v-if="error" class="muted">{{ error }}</p>
+          </tr>
+          </tbody>
+        </table>
       </div>
       <div class="col-md-4 plain-element"></div>
     </div>
   </div>
   <div v-else class="dashboard-cards">
-    <div class="row">
-      <div class="col-md-4 plain-element"></div>
-      <div class="col-md-4 plain-element">
-        <div class="card card-instrument">
-          <div class="card-content">
-            <h4> 404 - Page Not Found </h4>
+      <div class="row">
+        <div class="col-md-4 plain-element"></div>
+        <div class="col-md-4 plain-element">
+          <div class="card card-instrument">
+            <div class="card-content">
+              <h4> 404 - Page Not Found </h4>
+            </div>
           </div>
         </div>
       </div>
