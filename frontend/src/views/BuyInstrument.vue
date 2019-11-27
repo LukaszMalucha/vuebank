4f686bde0910a781c1c8379ed3976c6b49494fe5
unknown <lucasmalucha@gmail.com>
2019-11-27 06:55:26 +0000

    calc

--- frontend/src/views/BuyInstrument.vue
+++ frontend/src/views/BuyInstrument.vue
@@ -32,7 +32,10 @@
           </tr>
           <tr>
             <td>Quantity:</td>
-            <td class="cell-input"><input v-model="assetQuantity" type="number" placeholder="quantity" class="form-control form-control-transaction"/></td>
+            <td class="cell-input">
+              <input v-model="assetQuantity" type="number" placeholder="Specify quantity..."
+                      class="form-control form-control-transaction" id="quantityCounter"/>
+            </td>
           </tr>
           <br>
           <tr>
@@ -41,12 +44,45 @@
           </tr>
           </tbody>
         </table>
-
         <p v-if="error" class="muted">{{ error }}</p>
-         <button type="submit" class="btn btn-success">Buy</button>
+        <button type="submit" class="btn btn-confirm btn-success">Buy</button>
         </form>
       </div>
-      <div class="col-md-4 plain-element"></div>
+      <div class="col-md-4 plain-element">
+      <table>
+        <thead>
+        <td>Room Type</td>
+        <td>Adult</td>
+        <td>Children</td>
+        <td>Total Price</td>
+        </thead>
+                <tbody>
+                  <tr v-for="(row, index) in rows">
+                    <td>
+                      <select v-model="row.roomType">
+                        <option v-for="room in rooms" v-bind:value="room.title">{{room.title}}</option>
+                      </select>
+                    </td>
+                    <td>
+                      <select v-model="row.adultCount">
+                      <option value="1">1 Adult</option>
+                      <option value="2">2 Adults</option>
+                    </select>
+                  </td>
+                  <td>
+                    <select v-model="row.childCount">
+                    <option value="0">No Child</option>
+                    <option value="1">1 Child</option>
+                    <option value="2">2 Children</option>
+                  </select>
+                </td>
+                <td>
+                  {{calcRoomTotal(row)}}
+                </td>
+              </tr>
+    </tbody>
+  </table>
+      </div>
     </div>
   </div>
   <div v-else class="dashboard-cards">
@@ -87,10 +123,43 @@ export default {
       instrument: {},
       assetQuantity: null,
       error: null,
-
+      rows: [{
+      roomType : "Standard Room",
+      adultCount : 1,
+      childCount : 0
+      }],
+      rooms: [
+      {
+          value:0,
+          title: 'Standard Room'
+      },
+      {
+          value:0,
+          title: 'Deluxe Room'
+      },
+      ],
+      items: [{
+          price: 1000,
+      }]
     }
   },
   methods: {
+    addRoom: function() {
+      this.rows.push({
+      roomType : "Standard Room",
+      adultCount : 1,
+      childCount : 0
+      });
+    },
+
+    removeRoom: function(index) {
+      this.rows.splice(index, 1);
+    },
+
+    calcRoomTotal: function(row) {
+        return (parseInt(row.adultCount) + parseInt(row.childCount)) * 1000;
+    },
+
     setPageTitle(title) {
       document.title = title;
     },
